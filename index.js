2adb5394ea60a96a454ecde0276b75e4e67b6f61
Sajal <mostafizurrahamnsajal8@gmail.com>
2025-03-18 16:33:46 +0600

    --

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-const sdk = require("node-appwrite");
-
-module.exports = async function (context) {
-  const client = new sdk.Client()
-    .setEndpoint("https://cloud.appwrite.io/v1") // Replace with your Appwrite endpoint
-    .setProject(process.env.APPWRITE_ProjectId) // Replace with your Project ID
-    .setKey(process.env.APPWRITE_API_KEY); // Replace with your API key
-
-  const database = new sdk.Databases(client);
-
-  try {
-    const response = await database.listDocuments(
-      process.env.APPWRITE_databasesId,
-      process.env.APPWRITE_collectionId1
-    );
-    for (const doc of response.documents) {
-      await database.deleteDocument(
-        process.env.APPWRITE_databasesId,
-        process.env.APPWRITE_collectionId1,
-        doc.$id
-      );
-    }
-    context.log("All documents deleted successfully!");
-  } catch (error) {
-    context.error("Error deleting documents:", error);
-  }
+export default async ({ req, res, log, error }) => {
+  return res.send("hello world");
 };
