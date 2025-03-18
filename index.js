9f990ca1e6e62197d8c0dd12c481ac5853f90df3
Sajal <mostafizurrahamnsajal8@gmail.com>
2025-03-18 13:25:44 +0600

    --

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ const sdk = require("node-appwrite");
 module.exports = async function (context) {
   const client = new sdk.Client()
     .setEndpoint("https://cloud.appwrite.io/v1") // Replace with your Appwrite endpoint
-    .setProject(process.env.VITE_APPWRITE_ProjectId) // Replace with your Project ID
-    .setKey(process.env.APPWRITE_FUNCTION_API_KEY); // Replace with your API key
+    .setProject(process.env.APPWRITE_ProjectId); // Replace with your Project ID
+  // .setKey(process.env.APPWRITE_FUNCTION_API_KEY); // Replace with your API key
 
   const database = new sdk.Databases(client);
 
   try {
     const response = await database.listDocuments(
-      process.env.VITE_APPWRITE_databasesId,
-      process.env.VITE_APPWRITE_collectionId1
+      process.env.APPWRITE_databasesId,
+      process.env.APPWRITE_collectionId1
     );
     for (const doc of response.documents) {
       await database.deleteDocument(
-        process.env.VITE_APPWRITE_databasesId,
-        process.env.VITE_APPWRITE_collectionId1,
+        process.env.APPWRITE_databasesId,
+        process.env.APPWRITE_collectionId1,
         doc.$id
       );
     }
